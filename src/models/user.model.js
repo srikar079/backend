656561fcbbf6a56f63ee595013476705469b9d74
srikar079 <srikar079@gmail.com>
2024-10-15 00:04:45 +0530

    jwt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchema= new Schema(
     {
            username:{
@@ -57,4 +58,34 @@ userSchema.pre("Save", async function (next){
   this.password= bcrypt.hash(this.password,10)
   next();
 })
+
+
+
+userSchema.methods.generateAccessToken= function(){
+  jwt.sign({
+    _id:this._id,
+    email:this.email,
+    username:this.username,
+    fullname:this.fullname
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+  }
+)
+}
+
+
+userSchema.methods.generateRefreshToken= function(){
+  jwt.sign({
+    _id:this._id,
+  },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+  }
+)
+}
+
 export const User= mongoose.model("User",userSchema);
+
