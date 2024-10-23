7956eada7f07728de1f56a07361afeae4d1eb97c
srikar079 <srikar079@gmail.com>
2024-10-23 22:25:54 +0530

    .

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,9 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 
 const registerUser= asyncHandler(async(req,res)=>{
-    return res.status(200).json({
-        message:"ok"
-    })
+   if(fullName===""){
+    
+   }
 })
 
 export {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,17 @@ import {registerUser} from "../controllers/user.controller.js"
 
 const router=Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(\
+    XMLHttpRequestUpload.files([
+        {
+            name:"avatar",
+            maxCount:1 
+        },
+        {
+            name:"coverImage",
+            maxCount=1
+        }
+    ]),
+    registerUser);
 
 export default router;
