9e367db0e8332b64f2023b4a99ed0c059418f39f
srikar079 <srikar079@gmail.com>
2024-10-25 22:36:48 +0530

    .

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,28 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../"
 const registerUser= asyncHandler(async(req,res)=>{
+  
+   const {fullName, email, username, password}= req.body;
+   console.log("email:", email);
+      
    if(fullName===""){
-    
+      throw new ApiError(400, "Full name is required")
    }
+  if(
+   [fullName, email, username, password].some((field)=>filed?.trim()==="")
+     )
+  {
+   throw new ApiError(400, "All fields required")
+  }
+  
+  User.findOne({
+   $or: [{ username } , { email }]
+  })
+     if(existerUser){
+      throw new ApiError(409,"User with email or username exists")
+     }
+   
 })
 
 export {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,23 @@
-import {Router} from "express"
-import {registerUser} from "../controllers/user.controller.js"
+import {Router} from "express";
+import {registerUser} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js";
+
 
 const router=Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser
+);
 
-    const {fullName, email,username,password}=req.body;
-    
 
 export default router;
