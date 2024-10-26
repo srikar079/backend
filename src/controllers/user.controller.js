e17513e2e4f695aa2028b4c45e3c5570dbb77c3f
srikar079 <srikar079@gmail.com>
2024-10-26 21:05:02 +0530

    .

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
-import {User} from "../"
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/Cloudinary.js"
 const registerUser= asyncHandler(async(req,res)=>{
   
    const {fullName, email, username, password}= req.body;
@@ -16,13 +17,24 @@ const registerUser= asyncHandler(async(req,res)=>{
    throw new ApiError(400, "All fields required")
   }
   
-  User.findOne({
+  const existedUser=User.findOne({
    $or: [{ username } , { email }]
   })
-     if(existerUser){
+     if(existedUser){
       throw new ApiError(409,"User with email or username exists")
      }
    
+     const avatarLocalPath= req.files?.avatar[0]?.path;
+     const coverImageLocalPath= req.files?.coverImage[0]?.path;
+     path;
+
+     if(!avatarLocalPath){
+      throw new ApiError(400,"Avatar is required")
+     }
+
+      const avatar = await uploadOnCloudinary(avatarLocalPath)
+      const coverImage= await uploadOnCloudinary(coverImageLocalPath)
+
 })
 
 export {registerUser};
