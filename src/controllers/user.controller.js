44edf238a8be16d758cc86993c4e029b7c5e9e2b
srikar079 <srikar079@gmail.com>
2024-10-30 19:12:00 +0530

    register user setup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,8 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-import {ApiError} from "../utils/ApiError.js"
-import {User} from "../models/user.model.js"
-import {uploadOnCloudinary} from "../utils/Cloudinary.js"
+import {ApiError} from "../utils/ApiError.js";
+import {User} from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/Cloudinary.js";
+import {ApiResponse} from "../utils/ApiResponse";
 const registerUser= asyncHandler(async(req,res)=>{
   
    const {fullName, email, username, password}= req.body;
@@ -34,7 +35,29 @@ const registerUser= asyncHandler(async(req,res)=>{
 
       const avatar = await uploadOnCloudinary(avatarLocalPath)
       const coverImage= await uploadOnCloudinary(coverImageLocalPath)
+       
+      if(!avatar){
+         throw new ApiError(400, "Avatar file is required")
+      }
 
+     const user= await User.create({
+         fullName,
+         avatar: avatar.url,
+         coverImage: coverImage?.url || "",
+         email,
+         password,
+         username: username.toLowerCase()
+      })
+      const createdUser= await User.findById(user._id)
+
+      if(!createdUser){
+         throw new ApiError(500, "Error while registering the user");
+      }
+
+      return res.status(201).json({
+         new ApiResponse (200, createdUser, "Successfully registered");
+      })
+      // can directly send createdUser directly above
 })
 
 export {registerUser};
